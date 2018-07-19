1caf2a7638f5c579d8abf8141ef4c9a1192ae310
Lucas Marques <lucasmarques73@hotmail.com>
2018-07-19 15:49:17 -0300

    first page

--- src/api/main.go
+++ src/api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"	
+)
 
 func main(){
-	fmt.Println("Hello")
+	routes := mux.NewRouter()
+
+	routes.HandleFunc("/", HomeHandler)
+
+	port := ":80"
+	fmt.Println("Server running in port:", port)
+	log.Fatal(http.ListenAndServe(port, routes))
+}
+
+
+func HomeHandler (w http.ResponseWriter, r *http.Request){
+	t := template.Must(template.ParseFiles("views/index.html"))
+	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
