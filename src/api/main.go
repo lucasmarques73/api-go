fd59b27763daf8a8a088c758c2cea6dda5c204c7
Lucas Marques <lucasmarques73@hotmail.com>
2018-07-19 17:04:17 -0300

    Requests ok

--- src/api/main.go
+++ src/api/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"html/template"
 	"log"
-	"net/http"	
+	"net/http"
+	"users"
 )
 
 func main(){
-	routes := mux.NewRouter()
+	routes := mux.NewRouter().StrictSlash(true)
 
-	routes.HandleFunc("/", HomeHandler)
+	routes.HandleFunc("/", Home)
+	routes.HandleFunc("/users", getAllUsers).Methods("GET")
+	routes.HandleFunc("/users", createUser).Methods("POST")
+	routes.HandleFunc("/users/{id}", getUser).Methods("GET")
 
 	port := ":80"
 	fmt.Println("Server running in port:", port)
@@ -19,12 +25,57 @@ func main(){
 }
 
 
-func HomeHandler (w http.ResponseWriter, r *http.Request){
+func Home(w http.ResponseWriter, r *http.Request){
 	t := template.Must(template.ParseFiles("views/index.html"))
 	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
+
+func getAllUsers(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(users.ListUsers())
+}
+
+func createUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	var u users.User
+	
+	body, err := ioutil.ReadAll(r.Body)
+	checkErr(err)
+
+	err = r.Body.Close()
+	checkErr(err)
+
+	json.Unmarshal(body, &u)
+
+	u = users.CreateUser(u)
+
+	json.NewEncoder(w).Encode(u)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	id := mux.Vars(r)["id"]
+	json.NewEncoder(w).Encode(users.GetUserById(id))
+}
+
+// func updateUser(w http.ResponseWriter, r *http.Request){
+// 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	
+// 	id := mux.Vars(r)["id"]
+
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	checkErr(err)
+
+// 	err = r.Body.Close()
+// 	checkErr(err)
+
+// 	json.Unmarshal(body, &u)
+
+// 	json.NewEncoder(w).Encode(users.UpdateUser(u,id))
+// }
 
 func checkErr(err error) {
 	if err != nil {
---
+++ src/errors/erros.go
@@ -0,0 +1,7 @@
+package errors
+
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
